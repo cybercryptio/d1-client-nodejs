dc1e3cc64e834a95aeba7211500b8ed983b13e6e
Kim Christensen <kim@cyber-crypt.com>
2022-08-26 12:41:47 +0200

    Export base client too to get correct documentation

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 export { D1GenericClient } from './generic_client';
 export { D1StorageClient } from './storage_client';
+export { D1BaseClient } from './base_client';
 export { ICredentials } from './credentials/ICredentials';
 export { UsernamePasswordCredentials } from './credentials/UsernamePasswordCredentials';
 export { TokenCredentials } from './credentials/TokenCredentials';
